0c2b7ea085592c5070c2217b95cc6ad8c0f4af7d
moyudaiwang <ch8bw8k4p8>
2023-07-11 22:50:15 +0800

    donQuixotey-myVue-1.1.0 2023-07-11 22:49
    1.2.0修改首页排版（系统正常版）

--- src/views/loginMain.vue
+++ src/views/loginMain.vue
@@ -1,51 +1,41 @@
 <template>
-<div>
-  <body id="poster">
+  <div>
+    <body id="poster">
 
     <div class="background">
-     <img :src="imgSrc" width="100%" height="100%" alt="" />
+      <img :src="imgSrc" width="100%" height="100%" alt="" />
     </div>
     <div class="login">
-    <el-form class="login-container" label-position="left" label-width="0px">
-      <h3 class="login_title">LOGIN</h3>
-      <el-form-item>
-        <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
-      </el-form-item>
+      <el-form class="login-container" label-position="left" label-width="0px">
+        <h3 class="login_title">LOGIN</h3>
+        <el-form-item>
+          <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
+        </el-form-item>
 
-      <el-form-item>
-        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
-      </el-form-item>
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+        </el-form-item>
 
-      <el-form-item style="width: 100%">
-        <el-button type="primary" style="width: 100%;border: none" @click="login()">登 录</el-button>
-      </el-form-item>
-    </el-form>
+        <el-form-item style="width: 100%">
+          <el-button type="primary" style="width: 100%;border: none" @click="login()">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
-  </body>
+    </body>
   </div>
 </template>
 
 <script>
-import { setCookie,getCookie ,clearCookie } from '@/utils/login'
 export default {
   name: 'Login',
   data () {
     return {
       loginForm: {
-        username: 'C00000000001',
-        password: '123456'
+        username: '',
+        password: ''
       },
       responseResult: [],
       imgSrc: require('../assets/grild001.jpg')
-    }
-  },
-  mounted () {
-    if(getCookie("pas")&&getCookie("name")){ //判断页面是否有对应Cookie
-        this.loginForm.username=getCookie("name");             //有则写入
-        this.loginForm.password=getCookie("pas");
-    }else{
-        this.loginForm.username="";                            //无则写空
-        this.loginForm.password="";
     }
   },
   methods: {
@@ -58,14 +48,8 @@ export default {
       this.$axios.post(url, accountCodeEntity).then(response => {
         if (response.data.code == '100200') {
           this.$message({message: response.data.msg, type: 'success'})
-          //setCookie("name",this.loginForm.username,1);
-          //setCookie("pas",this.loginForm.password,1);
-          getCookie("name")
-          getCookie("pas")
           this.$router.replace('/home/homeMain')
         } else {
-          removeCookie("name");   //如没选中，则删除掉对应的Cookie
-          removeCookie("pas");   //如没选中，则删除掉对应的Cookie
           this.$message({message: response.data.msg, type: 'warning'})
         }
       }).catch(error => {
