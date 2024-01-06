937d41c749a118824b054c040486e69de36203e6
moyudaiwang <ch8bw8k4p8>
2024-01-07 00:23:28 +0800

    donQuixotey-myVue-1.3.0 2024-01-07 22:49
    1.3.0地图初始化

--- src/views/map/mapMain.vue
+++ src/views/map/mapMain.vue
@@ -1,44 +1,100 @@
 <template>
-  <div class="setting-app">
-    <baidu-map :center="center" class="bm-view" :zoom="zoom" style="height:700px" @click="getClickInfo" :scroll-wheel-zoom='true'></baidu-map>
+  <div>
+    <el-card style="height: 89vh">
+      <el-input placeholder="请输入地址" v-model="input3" class="input-with-select" >
+        <el-button slot="append" icon="el-icon-search" @click="inputfz" >搜索</el-button>
+      </el-input>
+      <!--
+    scroll-wheel-zoom是是否可以缩放
+    @ready是图加载完后触发事件
+    center是位置定位
+    zoom是缩放大小限制
+    inertial-dragging是允许惯性拖拽
+   -->
+      <baidu-map class="bm-view" :zoom="15" :center="center" inertial-dragging @ready="mapReady" :scroll-wheel-zoom="true">
+        <!-- 定位控件   anchor="BMAP_ANCHOR_BOTTOM_RIGHT"代表放在右下角 -->
+        <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
+        <!-- 地区检索  keyword：关键字搜索   @searchcomplete：检索完成后的回调函数   auto-viewport：检索结束后是否自动调整地图事业  -->
+        <bm-local-search :keyword="keyword" @searchcomplete="search" :auto-viewport="true" class="search"></bm-local-search>
+        <!-- 缩放控件   anchor代表控件停靠位置   anchor="BMAP_ANCHOR_TOP_RIGHT"代表放在右上角-->
+        <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+      </baidu-map>
+    </el-card>
   </div>
 </template>
 
 <script>
+//引入组件
+import {
+  BaiduMap,
+  BmControl,
+  BmView,
+  BmAutoComplete,
+  BmLocalSearch,
+  BmMarker,
+  BmGeolocation,
+} from "vue-baidu-map";
 export default {
-  name: 'BaiDuMap',
-  data () {
+  data() {
     return {
-      center: {lng: 109.45744048529967, lat: 36.49771311230842},
-      zoom: 13
-    }
+      //定位位置信息
+      center: {},
+      //检索关键字
+      keyword: "",
+      //输入框input值
+      input3: "",
+    };
+  },
+  //需要引入的组件
+  components: {
+    BaiduMap,
+    BmControl,
+    BmView,
+    BmAutoComplete,
+    BmLocalSearch,
+    BmMarker,
+    BmGeolocation,
   },
   methods: {
-    handler ({BMap, map}) {
-      var point = new BMap.Point(109.49926175379778, 36.60449676862417)
-      map.setMapStyleV2({
-        styleId: 'bc4a214961fd4c1dfaeda50453419828'
-      })
-      map.centerAndZoom(point, 13)
-      var marker = new BMap.Marker(point) // 创建标注
-      map.addOverlay(marker) // 将标注添加到地图中
-      var circle = new BMap.Circle(point, 6, { strokeColor: 'Red', strokeWeight: 6, strokeOpacity: 1, Color: 'Red', fillColor: '#f03' })
-      map.addOverlay(circle)
-      map.setMapType(BMAP_EARTH_MAP)
+    //输入框
+    inputfz() {
+      this.keyword = this.input3;
     },
-    getClickInfo (e) {
-      console.log(e.point.lng)
-      console.log(e.point.lat)
-      this.center.lng = e.point.lng
-      this.center.lat = e.point.lat
-    }
+    //地图加载后的回调
+    mapReady({ BMap, map }) {
+      //保存this指向，因为在百度的回调中this不指向vue
+      const _this = this;
+      // 获取自动定位方法
+      var geolocation = new BMap.Geolocation();
+      // 获取自动定位获取的坐标信息
+      geolocation.getCurrentPosition(
+        function (r) {
+          //可以conso.log看一下这个r，他里面包含了检索到的位置信息。下面就把两个维度信息赋值给center来定位
+          _this.center = {
+            lng: r.point.lng,
+            lat: r.point.lat,
+          };
+        },
+        //启用高精度
+        { enableHighAccuracy: true }
+      );
+    },
   },
-
-  mounted () {
-    document.querySelector('body').setAttribute('style')
-  }
-}
+};
 </script>
 
-<style>
+<style scoped>
+/* 给个宽高 */
+.bm-view {
+  height: 700px;
+  width: auto;
+}
+.input-with-select {
+  width: 400px;
+  margin-bottom: 5px;
+}
+.search {
+  height: 300px;
+  overflow: auto;
+}
 </style>
